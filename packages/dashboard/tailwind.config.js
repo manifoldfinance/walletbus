09d10cb2920f262d58babb73448a76829f49e56a
sam bacha <sam@manifoldfinance.com>
2022-03-07 15:25:49 -0800

    refactor(dashboard): fix runtime errors

--- packages/dashboard/tailwind.config.js
+++ packages/dashboard/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        oswald: ["Oswald"]
+        oswald: ["Inter"]
       },
       colors: {
         "truffle-blue": "#3fe0c5",
