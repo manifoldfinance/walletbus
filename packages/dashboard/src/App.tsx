97b5cee7fea4cfd79e94fa4144b79b55dc6bf788
sam bacha <sam@manifoldfinance.com>
2022-03-16 19:15:42 -0700

    feat(react): inital refactor

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,11 +1,23 @@
 import { getLibrary } from './utils/utils';
 import { Web3ReactProvider } from '@web3-react/core';
 import Dashboard from './Dashboard';
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import Layout from "./components/Layout";
+import ConnectButton from "./components/ConnectButton";
+import AccountModal from "./components/AccountModal";
+import "@fontsource/inter";
 
 function App() {
+const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+ <ChakraProvider>
+      <Layout>
+        <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+      </Layout>
       <Dashboard />
+      </ChakraProvider>
     </Web3ReactProvider>
   );
 }
